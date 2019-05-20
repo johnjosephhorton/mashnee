b9de1fedde7f2b410ff0a0f6790e821fcd23fe25
John Horton <john.joseph.horton@gmail.com>
2019-05-19 21:27:35 -0400

    Interface for entering properties

--- GG/__init__.py
+++ GG/__init__.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 
 import tempfile
+import json
 
 from shutil import copyfile
 from distutils.dir_util import copy_tree
@@ -9,11 +10,15 @@ from distutils.dir_util import copy_tree
 from flask import Flask
 from flask import render_template
 from flask import send_file
+from flask import request
+
+from trulia import GetData
 
 def root_dir():  # pragma: no cover
     return os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
 
 # Eventually, do these dynamically 
 static_dir = os.path.join(root_dir(), "static")
@@ -26,6 +31,10 @@ def hello():
     user = {'username': 'Robin'}
     return render_template('welcome.html', title = 'Home', user = user)
 
+@app.route("/enter_properties")
+def properties():
+    return render_template("enter_properties.html")
+
 @app.route("/report")
 def report():
     d = tempfile.mkdtemp(prefix='tmp')
@@ -35,7 +44,16 @@ def report():
     copyfile(os.path.join(d, "writeup/report.pdf"), os.path.join(static_dir, "report.pdf")) # copies out the report 
     static_file = os.path.join(static_dir,"report.pdf")
     return send_file(static_file, attachment_filename='report.pdf')
-        
+
+@app.route('/store_urls', methods=['POST'])
+def handle_data():
+    target_url = request.form['target_url']
+    target_data = GetData(target_url)
+    url_1 = request.form['comp_url_1']
+    #for url in projectpath:
+    #    print(url)
+    return json.dumps(target_data)
+    
 if __name__ == "__main__":
     app.run()
     
