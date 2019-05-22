bedbe169f76e4fa19084453c3b69307a49254e42
John Horton <john.joseph.horton@gmail.com>
2019-05-22 06:45:14 -0400

    Better looking x axis

--- report/analysis/plot_price.R
+++ report/analysis/plot_price.R
@@ -8,27 +8,44 @@ suppressPackageStartupMessages({
     library(reshape2)
 })
 
-# Load comparables data 
 
 source("get_data.R")
 
-## df.raw <- read.csv("../data/data.csv") %>% 
-##   mutate(price = gsub(",","",price) %>% as.numeric)
-
 df.raw$address <- with(df.raw, reorder(address, price,  mean))
+
+inches.per.row <- 0.25
+num.rows <- nrow(df.raw)
+
+priceFormatter <- function(...){
+    f <- function(x){
+        result <- ""
+    if (x < 1000000) {
+        result <- paste0("$", round(x / 1000, 0), "K")
+    } else {
+        result <- paste0("$", round(x / 1000000, 1), "M")
+    }
+        result
+    }
+    function(x) sapply(x, f)
+}
+
+#priceFormatter(1000)
 
 g <- ggplot(data = df.raw, aes(x = price, y = address, 
                                colour = factor(comp),
                                shape = factor(comp))) + 
-    geom_point() + 
+    geom_point(size = 2) + 
     theme_bw() + 
     theme(legend.position = "none") + 
-    xlab("Price") +
-    scale_x_continuous(labels = scales::dollar_format()) + 
+    xlab("(thousands)") +
+    scale_x_continuous(labels = priceFormatter()) + 
     geom_vline(data = df.raw %>% filter(comp == 0),
                aes(xintercept = price),
                colour = "red",
                linetype = "dashed") +
-    ylab("")
+    ylab("") +
+    xlab("")
 
-JJHmisc::writeImage(g, "price", width = 5, height = 2.5, path = "../writeup/plots/")
+print(g)
+
+JJHmisc::writeImage(g, "price", width = 4, height = inches.per.row * num.rows, path = "../writeup/plots/")
