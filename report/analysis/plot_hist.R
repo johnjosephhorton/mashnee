aeefd5bda1e99ad4f5d2dbd1665d12f9be0fa835
John Horton <john.joseph.horton@gmail.com>
2019-05-22 07:37:56 -0400

    Dot chart to show if in range

---
+++ report/analysis/plot_hist.R
@@ -0,0 +1,45 @@
+#! /usr/bin/env Rscript
+
+suppressPackageStartupMessages({
+    library(magrittr)
+    library(dplyr)
+    library(ggplot2)
+    library(ggrepel)
+    library(glmnet)
+    library(reshape2)
+    library(JJHmisc)
+})
+
+# Load comparables data 
+source("get_data.R")
+
+df.comps <- df.raw %>% select(address, comp, baths, bedrooms, square_feet, price, lotSize) %>% melt(id.vars = c("comp", "address"))
+
+pretty.labels <- list("baths" = "Baths",
+                      "bedrooms" = "Bedrooms",
+                      "square_feet" = "Living space",
+                      "lotSize" = "Lot size",
+                      "price" = "Price"
+                      )
+
+#df.combo$variable <- with(df.combo, as.character(sapply(variable, function(x) pretty.labels[as.character(x)])))
+
+df.comps %<>% group_by(variable) %>%
+    mutate(value = (value - min(value)) / max(value)) %>% ungroup
+
+g <- ggplot(data = df.comps, aes(x = value, fill = factor(comp))) +
+    facet_wrap(~variable, ncol = 1, scale = "free") +
+    geom_dotplot() + 
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90)) +
+    theme(legend.position = "none", strip.text.y = element_text(angle = 0)) +
+    ylab("% relative to target property") +
+    xlab("") 
+
+print(g)
+
+JJHmisc::writeImage(g, "hist", width = 5, height = 5, path = "../writeup/plots/")
+
+
+
+
